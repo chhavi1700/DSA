4f78eac09e057a73704cf0c24b340cd941916835
chhavi1700 <chhavi1700@gmail.com>
2026-07-04 20:48:54 +0530

    Time: 0 ms (100%), Space: 25.9 MB (78.31%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,29 +2,47 @@ class Solution {
 public:
     int trap(vector<int>& height) {
 
-        int n = height.size();
+        // int n = height.size();
 
-        vector<int> leftmax(n);
-        vector<int> rightmax(n);
+        // vector<int> leftmax(n);
+        // vector<int> rightmax(n);
 
-        int left = 0;
-        for(int i = 0; i < n; i++) {
-            left = max(left, height[i]);
-            leftmax[i] = left;
+        // int left = 0;
+        // for(int i = 0; i < n; i++) {
+        //     left = max(left, height[i]);
+        //     leftmax[i] = left;
+        // }
+
+        // int right = 0;
+        // for(int i = n - 1; i >= 0; i--) {
+        //     right = max(right, height[i]);
+        //     rightmax[i] = right;
+        // }
+
+        // int ans = 0;
+
+        // for(int i = 0; i < n; i++) {
+        //     ans += min(leftmax[i], rightmax[i]) - height[i];
+        // }
+
+        // return ans;
+        int l=0;
+        int r=height.size()-1;
+        int leftmax=0;
+        int rightmax=0;
+        int ans=0;
+        while(l<r){
+            leftmax=max(leftmax,height[l]);
+            rightmax=max(rightmax,height[r]);
+            if(leftmax<rightmax){
+                ans+=(leftmax-height[l]);
+                l++;
+            }
+            else {
+                ans+=(rightmax-height[r]);
+                r--;
+            }
         }
-
-        int right = 0;
-        for(int i = n - 1; i >= 0; i--) {
-            right = max(right, height[i]);
-            rightmax[i] = right;
-        }
-
-        int ans = 0;
-
-        for(int i = 0; i < n; i++) {
-            ans += min(leftmax[i], rightmax[i]) - height[i];
-        }
-
         return ans;
     }
 };
