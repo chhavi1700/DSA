961747aac813f942331aacdd6395e59877216568
chhavi1700 <chhavi1700@gmail.com>
2026-07-15 23:49:12 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-       int so=n*n;
-       int se=n*(n+1);
-        cout<<se<<" "<<so;
-        int r=0;
-        while(r>=0){
-            r=se%so;
-            if(r==0) {
-                return so;
-                break;}
-            se=so;
-            so=r;
+    //    int so=n*n;
+    //    int se=n*(n+1);
+    //     int r=0;
+    //     while(r>=0){
+    //         r=se%so;
+    //         if(r==0) {
+    //             return so;
+    //             break;}
+    //         se=so;
+    //         so=r;
             
-        }
-        return 0;
+    //     }
+    //     return 0;
+    return n;
     }
 };
