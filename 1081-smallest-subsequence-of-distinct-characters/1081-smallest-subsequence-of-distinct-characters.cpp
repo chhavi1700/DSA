75f02c2743b25c48f9fad58fb0cde8e2d303ccdf
chhavi1700 <chhavi1700@gmail.com>
2026-07-22 21:46:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (70.54%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -5,7 +5,6 @@ public:
         vector<int> last(26);
         vector<bool> vis(26, false);
 
-        // Store last occurrence of each character
         for (int i = 0; i < s.size(); i++) {
             last[s[i] - 'a'] = i;
         }
@@ -16,11 +15,8 @@ public:
 
             char c = s[i];
 
-            // Skip if already in the answer
             if (vis[c - 'a'])
                 continue;
-
-            // Remove larger characters if they appear later
             while (!st.empty() &&
                    st.back() > c &&
                    last[st.back() - 'a'] > i) {
