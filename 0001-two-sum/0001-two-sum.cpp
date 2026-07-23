f0b8648989d501fbcae612bcd499c396d6874b3d
chhavi1700 <chhavi1700@gmail.com>
2026-07-23 23:25:07 +0530

    Refactor twoSum function for clarity and efficiency

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
         map<int, int> mpp;
-
         for(int i = 0; i < nums.size(); i++) {
-
             int need = target - nums[i];
-
             if(mpp.find(need) != mpp.end()) {
                 return {mpp[need], i};
             }
-
             mpp[nums[i]] = i;
         }
-
         return {};
     }
 };
