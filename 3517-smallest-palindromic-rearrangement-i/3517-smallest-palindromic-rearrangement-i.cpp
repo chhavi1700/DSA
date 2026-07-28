4bf8ff20f54693451e56a3bbd9463ed4ce97d037
chhavi1700 <chhavi1700@gmail.com>
2026-07-29 00:14:19 +0530

    Time: 190 ms (7.03%), Space: 60.7 MB (81.54%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        // sort(s.begin(),s.end());
-        // string t=s;
-        // int n=s.size();
-        // int j=n-1;
-        // for (int i=0;i<n/2;i++){
-        //     t[i]=s[i];
-        //     cout<<t<<endl;
-        //     t[j]=s[i+1];
-        //      cout<<t<<endl;
-        //     j--;
-        //     i++;
-        // }
-        // return t;
-        
-        int n = s.size(), j = 0;
-        int freq[26]{};
-
-        // Traverse the first half
-        for (int i = 0; i < n >> 1; i++)            
-            freq[(s[i] & 31) - 1]++;      // ASCII → char freq index
-
-        for (int i = 0; i < 26; i++) {
-            while (freq[i]--) {                
-                s[j] = 'a' + i;           // construct 1st half →                
-                s[n - 1 - j++] = 'a' + i; // construct 2nd half ←
+        sort(s.begin(),s.end());
+        string t=s;
+        int n=s.size();
+        int i=0;
+        int j=n-1;
+        int k=0;
+               while (k < n) {
+            if (k + 1 < n && s[k] == s[k + 1]) {
+                t[i] = s[k];
+                t[j] = s[k];
+                i++;
+                j--;
+                k += 2;
+            } else {
+                // Middle character (odd frequency)
+                t[n / 2] = s[k];
+                k++;
             }
         }
 
-        return s;
+        return t;
+        
+        // int n = s.size(), j = 0;
+        // int freq[26]{};
+
+        // // Traverse the first half
+        // for (int i = 0; i < n >> 1; i++)            
+        //     freq[(s[i] & 31) - 1]++;      // ASCII → char freq index
+
+        // for (int i = 0; i < 26; i++) {
+        //     while (freq[i]--) {                
+        //         s[j] = 'a' + i;           // construct 1st half →                
+        //         s[n - 1 - j++] = 'a' + i; // construct 2nd half ←
+        //     }
+        // }
+
+        // return s;
     }
 };
