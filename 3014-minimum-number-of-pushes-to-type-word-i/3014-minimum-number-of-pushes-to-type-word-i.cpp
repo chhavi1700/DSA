626fcfaa57c438650dcdb7c10cc00068ea6a754e
chhavi1700 <chhavi1700@gmail.com>
2026-07-30 23:31:46 +0530

    Time: 3 ms (11.73%), Space: 9.6 MB (10.54%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+         unordered_map<char, int> freq;
+         int n=8;
+    // Count frequencies
+    for (char c : word)
+        freq[c]++;
+
+    // Store frequencies in a vector
+    vector<int> v;
+    for (auto it : freq)
+        v.push_back(it.second);
+
+    // Sort in descending order
+    sort(v.begin(), v.end(), greater<int>());
+
+    int ans = 0;
+    int multiplier = 1;
+
+    for (int i = 0; i < v.size(); i++) {
+        ans += v[i] * multiplier;
+
+        // Increase multiplier after every n frequencies
+        if ((i + 1) % n == 0)
+            multiplier++;
+    }
+
+    return ans;
+    }
+};
