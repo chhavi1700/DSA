d7cfcdf5d35c282051a77f4b3619f8e992ed5645
chhavi1700 <chhavi1700@gmail.com>
2026-07-30 23:34:08 +0530

    Time: 0 ms (100%), Space: 8.2 MB (95.83%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,31 +1,9 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-         unordered_map<char, int> freq;
-         int n=8;
-    // Count frequencies
-    for (char c : word)
-        freq[c]++;
-
-    // Store frequencies in a vector
-    vector<int> v;
-    for (auto it : freq)
-        v.push_back(it.second);
-
-    // Sort in descending order
-    sort(v.begin(), v.end(), greater<int>());
-
-    int ans = 0;
-    int multiplier = 1;
-
-    for (int i = 0; i < v.size(); i++) {
-        ans += v[i] * multiplier;
-
-        // Increase multiplier after every n frequencies
-        if ((i + 1) % n == 0)
-            multiplier++;
-    }
-
-    return ans;
+        int n = word.length();
+        int blocks = n / 8;
+        return (blocks * (blocks + 1) * 4)
+                + (n % 8) * (blocks + 1);
     }
 };
