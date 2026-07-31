9e7c830f943c137c83ac18df4ddcb10cdca44721
chhavi1700 <chhavi1700@gmail.com>
2026-07-31 22:28:22 +0530

    Time: 18 ms (31.1%), Space: 41 MB (5.99%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -3,25 +3,28 @@ public:
     int minimumPushes(string word) {
         int freq[26] = {0};
 
-// Count frequency of each character
-for (char c : word)
-    freq[c - 'a']++;
+        for (char c : word)
+            freq[c - 'a']++;
 
-// Append frequencies to a vector
-vector<int> v;
+        // bucket[f] = number of characters having frequency f
+        vector<int> bucket(word.size() + 1, 0);
 
-for (int i = 0; i < 26; i++) {
-    if (freq[i] > 0)
-        v.push_back(freq[i]);
-}
-int sum=0;
-sort(v.begin(),v.end(),greater<int>());
-        for (int i = 0; i < v.size(); i++) {
-    if (i < 8) sum += v[i];
-    else if (i < 16) sum += v[i] * 2;
-    else if (i < 24) sum += v[i] * 3;
-    else sum += v[i] * 4;
-}
-        return sum;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0)
+                bucket[freq[i]]++;
+        }
+
+        int ans = 0;
+        int pos = 0;
+
+        // Traverse frequencies from highest to lowest
+        for (int f = word.size(); f >= 1; f--) {
+            while (bucket[f]--) {
+                ans += f * (pos / 8 + 1);
+                pos++;
+            }
+        }
+
+        return ans;
     }
 };
