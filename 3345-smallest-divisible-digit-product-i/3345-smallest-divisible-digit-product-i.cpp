f303ec48ebb451ea63c62cfc86967f6f0afc3fd6
chhavi1700 <chhavi1700@gmail.com>
2026-08-06 23:39:12 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.77%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int smallestNumber(int n, int t) {
-        int a=n; 
+         
         while(true){   
              int p=1;
-             int temp=a;
+             int temp=n;
             while(temp){
                 int r=temp%10;
                 p*=r;
                 temp/=10;
                 }
             if(p%t==0) {
-                return a;
-                break;}
-            else a++; 
+                return n;
+                }
+            else n++; 
         }
     }
 };
